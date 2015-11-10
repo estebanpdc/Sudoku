77dd5f4093d023f8285e128f03324383e79c0ea3
Jesús <chus_escavy95@hotmail.com>
2015-11-10 16:07:42 +0100

    buenos dias

---
+++ src/dominio/ControladorCreacionTablero.java
@@ -0,0 +1,11 @@
+package dominio;
+
+public class ControladorCreacionTablero {
+	private Tablero miTablero;
+	
+	public ControladorCreacionTablero() {}
+	
+	public ControladorCreacionTablero(Tablero t) {
+		this.miTablero = t;
+	}
+}
