5d0d33adeae4b56100df2a3c93a32ed51b703815
Jesús <chus_escavy95@hotmail.com>
2015-11-10 21:56:14 +0100

    Jesus

--- src/dominio/ControladorFicheros.java
+++ src/dominio/ControladorFicheros.java
@@ -1,10 +1,27 @@
 package dominio;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class ControladorFicheros {
 	
 	public ControladorFicheros (){
 		
 	}
 	
-	public 
+	public void guardarUsuario(String nombrefichero, String usuario, String password) throws IOException {
+		File archivo = new File(nombrefichero);
+		FileWriter fw = new FileWriter (archivo);
+		PrintWriter pr = new PrintWriter(fw);
+		pr.println(usuario);
+		pr.println(password);
+		pr.close();
+	}
 }
