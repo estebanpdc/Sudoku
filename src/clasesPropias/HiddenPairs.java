2db2bf586a1ec99220a6b8e58b32bf88459b88c6
Jesús <chus_escavy95@hotmail.com>
2015-11-12 17:52:15 +0100

    nem fent

--- src/clasesPropias/HiddenPairs.java
+++ src/clasesPropias/HiddenPairs.java
@@ -2,6 +2,32 @@ package clasesPropias;
 
 public class HiddenPairs {
 	public void hiddenPairs(Tablero t) {
-			
+			int n = t.getMida();
+			for(int i=0; i<n*n; ++i){
+				for(int j=0; j<n*n; ++j){
+					boolean res[] = t.getCandidatsTauler(i, j);
+					res = comprobarFila(t,i,j,res);
+				}
+			}
+	}
+	
+	private static boolean[] comprobarFila(Tablero t, int i, int j, boolean[] res) {
+		int n = t.getMida();
+		boolean aux[];
+		boolean aux2[];
+		for(int k = 0; k < n*n; ++k) {
+			if (k != i) {			
+				aux = t.getCandidatsTauler(i, k);
+				for(int it = 0; it < n*n; ++it) {
+					if (res[it] != aux[it]) res[it] = false;
+				}
+				for(int it2 = 0; it2 < n*n; ++it2) {
+					if(it2 != i && it2 != k) {
+						
+					}
+				}
+			}
+		}
+		return res;
 	}
 }
